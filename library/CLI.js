1f73b0a4e0eb7d573bffdf1b98878783384441fd
Nathan Lane <lanenathan214@gmail.com>
2024-04-14 20:29:38 -0400

    imported shape classes

--- library/CLI.js
+++ library/CLI.js
@@ -2,7 +2,7 @@ const inquirer = require("inquirer");
 const {writeFile} = require("fs/promises");
 const choices = require("inquirer/lib/objects/choices");
 const SVG = require("./svg");
-
+const { Circle, Triangle, Square } = require("./shapes");
 
 
 class CLI {
