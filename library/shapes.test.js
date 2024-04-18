0ebd18776c3076889983558ab87725b14a884d81
Nathan Lane <lanenathan214@gmail.com>
2024-04-17 22:45:21 -0400

    fixed spacing at the end of expected code

--- library/shapes.test.js
+++ library/shapes.test.js
@@ -2,14 +2,14 @@ const {  Triangle, Circle, Square } = require("./shapes");
 
 describe("Circle", () => {
     test("should render svg for a blue circle element", () => {
-      const expectedSvg = '<circle cx="150" cy="100" r="80" fill="blue" />';
+      const expectedSvg = '<circle cx="150" cy="100" r="80" fill="blue"/>';
       const circle = new Circle();
       circle.setColor("blue");
       const actualSvg = circle.render();
       expect(actualSvg).toEqual(expectedSvg);
     });
     test("should accept a fillColor param", () => {
-      const expectedSvg = '<circle cx="150" cy="100" r="80" fill="red" />';
+      const expectedSvg = '<circle cx="150" cy="100" r="80" fill="red"/>';
       const circle = new Circle();
       circle.setColor("red");
       const actualSvg = circle.render();
@@ -19,14 +19,14 @@ describe("Circle", () => {
 
   describe("Square", () => {
     test("should render svg for a blue square element", () => {
-      const expectedSvg = '<rect x="90" y="40" width="120" height="120" fill="blue" />';
+      const expectedSvg = '<rect x="90" y="40" width="120" height="120" fill="blue"/>';
       const square = new Square();
       square.setColor("blue");
       const actualSvg = square.render();
       expect(actualSvg).toEqual(expectedSvg);
     });
     test("should accept a fillColor param", () => {
-      const expectedSvg = '<rect x="90" y="40" width="120" height="120" fill="yellow"  />';
+      const expectedSvg = '<rect x="90" y="40" width="120" height="120" fill="yellow"/>';
       const square = new Square();
       square.setColor("yellow");
       const actualSvg = square.render();
@@ -45,7 +45,7 @@ describe("Circle", () => {
     test("should accept a fillColor param", () => {
       const expectedSvg = '<polygon points="150, 18 244, 182 56, 182" fill="black"/>';
       const triangle = new Triangle();
-      circle.setColor("black");
+      triangle.setColor("black");
       const actualSvg = triangle.render();
       expect(actualSvg).toEqual(expectedSvg);
     });
